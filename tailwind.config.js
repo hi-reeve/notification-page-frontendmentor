19ae90db5a9092151f36dc21758a26fcbd80110b
Christopher Reeve <reeve@Christophers-MacBook-Pro.local>
2022-12-07 12:03:16 +0700

    style(main): add variable color and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'plus-jakarta-sans': ['Plus Jakarta Sans', 'sans-serif'],
+			},
+			colors: {
+				primary: {
+					red: 'hsl(var(--primary-red) / <alpha-value>)',
+					blue: 'hsl(var(--primary-blue) / <alpha-value>)',
+				},
+				neutral: {
+					'light-grayish-blue-1':
+						'hsl(var(--light-grayish-blue-1) / <alpha-value>)',
+					'light-grayish-blue-2':
+						'hsl(var(--light-grayish-blue-2) / <alpha-value>)',
+					'light-grayish-blue-3':
+						'hsl(var(--light-grayish-blue-3) / <alpha-value>)',
+					'grayish-blue': 'hsl(var(--grayish-blue) / <alpha-value>)',
+					'dark-grayish-blue':
+						'hsl(var(--dark-grayish-blue) / <alpha-value>)',
+					'very-dark-blue':
+						'hsl(var(--very-dark-blue) / <alpha-value>)',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
